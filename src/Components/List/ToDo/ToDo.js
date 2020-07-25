3c3cc4cb86972a153d9803525dc65569b28b3fa2
LenaYork <lenayork@tut.by>
2020-07-25 17:56:13 +0300

    feat: add delete to do button

--- src/Components/List/ToDo/ToDo.js
+++ src/Components/List/ToDo/ToDo.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './ToDo.css';
 import { Radio } from 'antd';
+import { Button } from 'antd';
 export function ToDo(props) {
     
     return(
         <div className="todo">
-            <Radio /> {props.text}
+            <Radio /> 
+            {props.text} 
+            <Button className="delete-todo-button" type="danger" size="small" shape="round" color="purple-7"  onClick={() => {}}>
+                x
+            </Button>
         </div>
     )
 }

d7af41910737f520382fb5340ab62a00d0d5224e
LenaYork <lenayork@tut.by>
2020-07-26 19:40:30 +0300

    feat: add button tooltips

--- src/Components/Control/Control.js
+++ src/Components/Control/Control.js
@@ -1,36 +1,63 @@
+import './Control.css';
+
 import React from 'react';
-import {Button} from 'antd';
-import {InputNumber} from 'antd';
+import {Button, InputNumber, Tooltip} from 'antd';
 import { PlusCircleOutlined, MinusCircleOutlined } from '@ant-design/icons';
 // import { purple } from '@ant-design/colors';
-import './Control.css';
+
+
+const addTip = <span>add your new item</span>;
+const deleteTip = <span>delete your new item</span>;
+const totalTip = <span>total in chosen category</span>;
+const doneTip = <span>show only implemented items</span>;
+const pendingTip = <span>show only pending items</span>;
+const allTip = <span>show all items</span>;
+const clearTip = <span>delete all items</span>;
+
 
 export function Control() {
     return (
         <div className="control-container">
             {/* Here will lay the buttons */}
-            <Button className="control-button" type="ghost" size="middle" color="purple-7" shape="round" onClick={() => {}}>
-                 <PlusCircleOutlined />
-            </Button>
+            <Tooltip placement="top" title={addTip}>
+                <Button className="control-button" type="ghost" size="middle" color="purple-7" shape="round" onClick={() => {}}>
+                    <PlusCircleOutlined className="plus-button"style={{ fontSize: '16px', color: '#08c' }}/>
+                </Button>
+            </Tooltip>
 
-            <Button className="control-button" type="ghost" size="middle" color="purple-7" shape="round" onClick={() => {}}>
-                 <MinusCircleOutlined />
-            </Button>
+            <Tooltip placement="top" title={deleteTip}>
+                <Button className="control-button" type="ghost" size="middle" color="purple-7" shape="round" onClick={() => {}}>
+                    <MinusCircleOutlined className="minus-button"/>
+                </Button>
+            </Tooltip>
 
-            <InputNumber className="user-quantity" defaultValue={5} disabled='true' id="user-quantity" />
+            <Tooltip placement="top" title={totalTip}>    
+                <InputNumber className="user-quantity" defaultValue={5} disabled='true' id="user-quantity" />
+            </Tooltip>
 
-            <Button className="control-button" type="ghost" size="middle" color="purple-7" shape="round" onClick={() => {}}>
-                DONE
-            </Button>
-            <Button className="control-button" type="ghost" size="middle" color="purple-7" shape="round" onClick={() => {}}>
-                PENDING
-            </Button>
-            <Button className="control-button" type="ghost" size="middle" color="purple-7" shape="round" onClick={() => {}}>
-                ALL
-            </Button>
-            <Button className="control-button" type="danger" size="middle" color="purple-7" shape="round" onClick={() => {}}>
-                DEL
-            </Button>
+            <Tooltip placement="top" title={doneTip}>
+                <Button className="control-button" type="ghost" size="middle" color="purple-7" shape="round" onClick={() => {}}>
+                    DONE
+                </Button>
+            </Tooltip>
+
+            <Tooltip placement="top" title={pendingTip}>
+                <Button className="control-button" type="ghost" size="middle" color="purple-7" shape="round" onClick={() => {}}>
+                    PENDING
+                </Button>
+            </Tooltip>
+
+            <Tooltip placement="top" title={allTip}>
+                <Button className="control-button" type="ghost" size="middle" color="purple-7" shape="round" onClick={() => {}}>
+                    ALL
+                </Button>
+            </Tooltip>
+            
+            <Tooltip placement="top" title={clearTip}>
+                <Button className="control-button" type="danger" size="middle" color="purple-7" shape="round" onClick={() => {}}>
+                    DEL
+                </Button>
+            </Tooltip>
         </div>
     )
 }
